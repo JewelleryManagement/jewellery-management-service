965d0b376891fb7fc494ee1789c338387e1e473e
dimitrov93 <ts.dimitrov93@gmail.com>
2023-07-06 12:04:27 +0300

    Initial BE skeleton setup
    GET to /home

---
+++ jewellery-inventory/src/main/java/com/example/jewelleryinventory/Home.java
@@ -0,0 +1,13 @@
+package com.example.jewelleryinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home.class, args);
+	}
+
+}
---
+++ jewellery-inventory/src/test/java/com/example/jewelleryinventory/JewelleryInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jewelleryinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
