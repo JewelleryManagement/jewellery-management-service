f591dcf4399be3f9e5c8c932eaa2b69e4a4747c9
dimitrov93 <86495759+dimitrov93@users.noreply.github.com>
2023-07-07 12:45:33 +0300

    Feature 3 create be skeleton (#4)
    * Initial BE skeleton setup
    
    * re-applying initial setup

---
+++ src/test/java/jewellery/inventory/JewelleryInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package jewellery.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
