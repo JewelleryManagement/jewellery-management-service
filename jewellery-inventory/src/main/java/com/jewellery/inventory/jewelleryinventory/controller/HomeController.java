5cf0b2b2d4eb40c1310fdd2c0660873563d538fd
dimitrov93 <ts.dimitrov93@gmail.com>
2023-07-07 09:48:04 +0300

    Initial BE skeleton setup

---
+++ jewellery-inventory/src/main/java/com/jewellery/inventory/jewelleryinventory/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jewellery.inventory.jewelleryinventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello world";
+    }
+}
