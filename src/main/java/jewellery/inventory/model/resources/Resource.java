168e27d30a1022c47c1dd69773e7b318e88866a7
dimitrov93 <86495759+dimitrov93@users.noreply.github.com>
2023-07-10 09:37:11 +0300

    Adding core entities + lombok + color has been removed from Resources (#7)
    * Adding core entities + lombok + color has been removed from Resources
    
    * updated pearl + main file
    
    ---------
    
    Co-authored-by: Aleksander Pavlov <alekseybg@gmail.com>

---
+++ src/main/java/jewellery/inventory/model/resources/Resource.java
@@ -0,0 +1,27 @@
+package jewellery.inventory.model.resources;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Resource {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private String quantityType;
+
+    @OneToMany(mappedBy = "resource", cascade = CascadeType.ALL)
+    private List<ResourceInUser> userAffiliations;
+
+    @OneToMany(mappedBy = "resource", cascade = CascadeType.ALL)
+    private List<ResourceInProduct> productAffiliations;
+}
