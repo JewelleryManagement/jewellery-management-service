7d6ea0df96fddf6588766a93aa80e52f0e44447d
Alekseeybg <alekseybg@gmail.com>
2023-07-12 09:47:58 +0300

    [Feature-5-setup-db] Reverted JewelleryInventoryApplication to the version on the Test branch.

--- src/main/java/jewellery/inventory/JewelleryInventoryApplication.java
+++ src/main/java/jewellery/inventory/JewelleryInventoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JewelleryInventoryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JewelleryInventoryApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JewelleryInventoryApplication.class, args);
+	}
 
 }
