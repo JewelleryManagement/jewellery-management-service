0be4df1435c5e9b61a2e498e90a37ef34893eaf2
Vladislav Katrankiev <vladokat@Vladislavs-MacBook-Air.local>
2023-07-18 17:01:41 +0300

    feature 0/ reformat code with google-java-format

--- src/main/java/jewellery/inventory/model/resources/Gemstone.java
+++ src/main/java/jewellery/inventory/model/resources/Gemstone.java
@@ -1,20 +1,19 @@
 package jewellery.inventory.model.resources;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
-
-import jakarta.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
 public class Gemstone extends Resource {
-    private String color;
-    private double carat;
-    private String cut;
-    private String clarity;
-    private double dimensionX;
-    private double dimensionY;
-    private double dimensionZ;
-    private String shape;
+  private String color;
+  private double carat;
+  private String cut;
+  private String clarity;
+  private double dimensionX;
+  private double dimensionY;
+  private double dimensionZ;
+  private String shape;
 }
