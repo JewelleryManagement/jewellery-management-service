02e075b484b220eadf805f0d06f1ba44c456176f
Vladislav Katrankiev <vladokat@Vladislavs-MacBook-Air.local>
2023-07-19 15:01:49 +0300

    feature-6/ add unhappy tests for ResourceService

--- src/test/java/jewellery/inventory/unit/service/ResourceServiceTest.java
+++ src/test/java/jewellery/inventory/unit/service/ResourceServiceTest.java
@@ -1,6 +1,17 @@
 package jewellery.inventory.unit.service;
 
+import static jewellery.inventory.util.TestUtil.getGemstone;
+import static jewellery.inventory.util.TestUtil.provideResources;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import jewellery.inventory.dto.ResourceDTO;
+import jewellery.inventory.exeption.ApiRequestException;
 import jewellery.inventory.model.resource.Resource;
 import jewellery.inventory.repository.ResourceRepository;
 import jewellery.inventory.service.ResourceService;
@@ -11,14 +22,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
-import java.util.Optional;
-
-import static jewellery.inventory.util.TestUtil.provideResources;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ResourceServiceTest {
@@ -64,11 +67,47 @@ class ResourceServiceTest {
     when(resourceRepository.findById(resourceFromDatabase.getId()))
         .thenReturn(Optional.of(resourceFromDatabase));
     when(resourceRepository.save(any())).thenReturn(resourceFromDatabase);
+
     ResourceDTO actualResourceDto =
         resourceService.updateResource(expectedDto.getId(), expectedDto);
 
     verify(resourceRepository, times(1)).findById(any());
     verify(resourceRepository, times(1)).save(any());
     assertEquals(expectedDto, actualResourceDto);
+  }
+
+  @Test
+  void willDeleteAResource() {
+    Resource resource = getGemstone();
+    when(resourceRepository.findById(any())).thenReturn(Optional.ofNullable(resource));
+
+    resourceService.deleteResourceById(resource.getId());
+
+    verify(resourceRepository, times(1)).delete(resource);
+  }
+
+  @Test
+  void willThrowWhenDeleteANonExistingResource() {
+    when(resourceRepository.findById(any())).thenReturn(Optional.empty());
+
+    assertThrows(
+        ApiRequestException.class, () -> resourceService.deleteResourceById(UUID.randomUUID()));
+  }
+
+  @Test
+  void willThrowWhenUpdateANonExistingResource() {
+    when(resourceRepository.findById(any())).thenReturn(Optional.empty());
+
+    assertThrows(
+        ApiRequestException.class,
+        () -> resourceService.updateResource(UUID.randomUUID(), ResourceDTO.builder().build()));
+  }
+
+  @Test
+  void willThrowWhenGetANonExistingResource() {
+    when(resourceRepository.findById(any())).thenReturn(Optional.empty());
+
+    assertThrows(
+        ApiRequestException.class, () -> resourceService.getResourceById(UUID.randomUUID()));
   }
 }
