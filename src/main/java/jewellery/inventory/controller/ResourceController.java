0a287ed1ed09cc9d9dca38051ce7b78803fbdac6
Vladislav Katrankiev <vladokat@Vladislavs-MacBook-Air.local>
2023-07-25 18:01:43 +0300

    feature-6/ change returns objects in controller

--- src/main/java/jewellery/inventory/controller/ResourceController.java
+++ src/main/java/jewellery/inventory/controller/ResourceController.java
@@ -1,10 +1,8 @@
 package jewellery.inventory.controller;
 
 import jakarta.validation.Valid;
-import java.net.URI;
 import java.util.List;
 import java.util.UUID;
-
 import jewellery.inventory.dto.request.resource.ResourceRequestDto;
 import jewellery.inventory.dto.response.resource.ResourceResponseDto;
 import jewellery.inventory.service.ResourceService;
@@ -13,15 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/resources")
 @RequiredArgsConstructor
 public class ResourceController {
 
-  @Autowired
-  private final ResourceService resourceService;
+  @Autowired private final ResourceService resourceService;
 
   @GetMapping
   public ResponseEntity<List<ResourceResponseDto>> getAllResources() {
@@ -35,25 +31,23 @@ public class ResourceController {
 
   @PostMapping
   public ResponseEntity<ResourceResponseDto> createResource(
-          @RequestBody @Valid ResourceRequestDto resourceRequestDto, UriComponentsBuilder uriComponentsBuilder) {
-    URI location =
-        uriComponentsBuilder
-            .path("/resources/{id}")
-            .buildAndExpand(resourceService.createResource(resourceRequestDto).getId())
-            .toUri();
-    return ResponseEntity.created(location).build();
+      @RequestBody @Valid ResourceRequestDto resourceRequestDto) {
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(resourceService.createResource(resourceRequestDto));
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<ResourceResponseDto> updateResource(
       @PathVariable("id") UUID id, @Valid @RequestBody ResourceRequestDto resourceRequestDto) {
-    return ResponseEntity.ok(resourceService.updateResource(id, resourceRequestDto));
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(resourceService.updateResource(id, resourceRequestDto));
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<String> deleteResourceById(@PathVariable("id") UUID id) {
     resourceService.deleteResourceById(id);
-    return new ResponseEntity<>(
-        "Resource with id: " + id + " has been deleted successfully!", HttpStatus.OK);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Resource with id: " + id + " has been deleted successfully!");
   }
 }
