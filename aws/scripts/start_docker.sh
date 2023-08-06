df69ad1e446cf853809a972eb906ee3f8308074b
Vladislav Katrankiev <vladokatr@gmail.com>
2023-08-06 19:30:20 +0300

    feature-ci-cd/ remove buildspec.yml. add secrets fetching to start_docker.sh

--- aws/scripts/start_docker.sh
+++ aws/scripts/start_docker.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Set the environment variables for your application
+JMS_DATABASE_NAME=$(aws ssm get-parameter --name JMS_DATABASE_NAME --query "Parameter.Value" --output text)
+JMS_DATABASE_USER=$(aws ssm get-parameter --name JMS_DATABASE_USER --query "Parameter.Value" --output text)
+JMS_DATABASE_PASSWORD=$(aws ssm get-parameter --name JMS_DATABASE_PASSWORD --query "Parameter.Value" --output text)
+
+export JMS_DATABASE_NAME="$JMS_DATABASE_NAME"
+export JMS_DATABASE_USER="$JMS_DATABASE_USER"
+export JMS_DATABASE_PASSWORD="$JMS_DATABASE_PASSWORD"
+
 docker-compose --file /tmp/docker-compose.yml up --build #> /logs/startup.log
