b92769094746d9db20157ac71b0c09278e482d77
Vladislav Katrankiev <vladokatr@gmail.com>
2023-08-06 19:45:14 +0300

    feature-ci-cd/ start docker daemon before reunning docker-compose

--- aws/scripts/start_docker.sh
+++ aws/scripts/start_docker.sh
@@ -9,4 +9,5 @@ export JMS_DATABASE_NAME="$JMS_DATABASE_NAME"
 export JMS_DATABASE_USER="$JMS_DATABASE_USER"
 export JMS_DATABASE_PASSWORD="$JMS_DATABASE_PASSWORD"
 
+service docker start
 docker-compose --file /tmp/docker-compose.yml up --build #> /logs/startup.log
