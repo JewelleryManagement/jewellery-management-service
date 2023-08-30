ca05b07f0f130817310fcd5602b7f83fe4a57e9e
Vladislav Katrankiev <vladokatr@gmail.com>
2023-08-30 16:27:27 +0300

    [feature-ci-cd] remove stop_docker_containers script

--- aws/scripts/stop_docker_containers.sh
+++ aws/scripts/stop_docker_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose --file /tmp/be/docker-compose.yml down --remove-orphans #>> /logs/stop.log
+#docker-compose --file /tmp/be/docker-compose.yml down --remove-orphans #>> /logs/stop.log
