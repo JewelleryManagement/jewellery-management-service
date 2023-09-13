8130a0f4c6cfcddbdf22ff773207eba65a124c82
Alekseeybg <alekseybg@gmail.com>
2023-09-13 17:18:02 +0300

    [Feature-19-implement-security][WIP] More appropriate error 403 Forbidden to 401 Unauthorized handling logic

---
+++ src/main/java/jewellery/inventory/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package jewellery.inventory.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+    throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
--- src/main/java/jewellery/inventory/security/SecurityConfig.java
+++ src/main/java/jewellery/inventory/security/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -47,7 +46,7 @@ public class SecurityConfig {
         .formLogin(formLogin -> formLogin.disable())
         .exceptionHandling(
             exceptionHandling ->
-                exceptionHandling.authenticationEntryPoint(new BasicAuthenticationEntryPoint()))
+                exceptionHandling.authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
         .cors(Customizer.withDefaults())
         .sessionManagement(
             (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
