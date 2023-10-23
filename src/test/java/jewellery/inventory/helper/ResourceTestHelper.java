9c4c8091c70bdefc4974d04e9e206adc8c76ab25
berki0 <107307903+berki0@users.noreply.github.com>
2023-10-23 19:40:34 +0300

    formatting

--- src/test/java/jewellery/inventory/helper/ResourceTestHelper.java
+++ src/test/java/jewellery/inventory/helper/ResourceTestHelper.java
@@ -31,7 +31,7 @@ public class ResourceTestHelper {
   private static final String PLATING = "silver";
   public static final String UPDATED_PLATING = PLATING + "ish";
   private static final String CUT = "diamond";
-  private static final String DIMENSION_SIZE="4.50x4.90x2.50";
+  private static final String DIMENSION_SIZE = "4.50x4.90x2.50";
   private static final Double DIMENSION_X = 4.5;
   private static final Double DIMENSION_Y = 4.9;
   private static final Double DIMENSION_Z = 2.5;
@@ -183,16 +183,16 @@ public class ResourceTestHelper {
 
   public static ResourceResponseDto getGemstoneResponseDto() {
     return GemstoneResponseDto.builder()
-            .id(RESOURCE_ID)
-            .clazz(CLAZZ_GEMSTONE)
-            .quantityType(QUANTITY_TYPE_UNIT)
-            .carat(CARAT_GEMSTONE)
-            .color(COLOR_GEMSTONE)
-            .cut(CUT)
-            .clarity(CLARITY)
-            .size(DIMENSION_SIZE)
-            .shape(SHAPE_GEMSTONEL)
-            .build();
+        .id(RESOURCE_ID)
+        .clazz(CLAZZ_GEMSTONE)
+        .quantityType(QUANTITY_TYPE_UNIT)
+        .carat(CARAT_GEMSTONE)
+        .color(COLOR_GEMSTONE)
+        .cut(CUT)
+        .clarity(CLARITY)
+        .size(DIMENSION_SIZE)
+        .shape(SHAPE_GEMSTONEL)
+        .build();
   }
 
   public static ResourceRequestDto getGemstoneRequestDto() {
