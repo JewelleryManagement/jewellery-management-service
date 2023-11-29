0e488dcb34951cff2d800a9782283029c7c72347
Alekseeybg <alekseybg@gmail.com>
2023-11-29 14:41:34 +0200

    [feature-42-user-details-extension] Refactor regex patterns in UserRequestDto. Replace PHONE_REGEXP with PHONE_REGEX and update name validation to use NAME_REGEX. Fix code smells pointed by sonar.

--- src/main/java/jewellery/inventory/dto/request/UserRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/UserRequestDto.java
@@ -17,18 +17,19 @@ public class UserRequestDto {
       "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be at least 8 characters long";
   private static final String PHONE_PATTERN_VALIDATION_MSG =
       "Phone number should be valid bulgarian mobile number";
-  private static final String PHONE_REGEXP = "^(\\+359|0)8[789]\\d{1}(-)?\\d{3}(-)?\\d{3}$";
+  private static final String NAME_REGEX = "^(?!.*__)[\\w]*$";
+  private static final String PHONE_REGEX = "^(\\+359|0)8[789]\\d(-)?\\d{3}(-)?\\d{3}$";
   private static final String ADDRESS_PATTERN_VALIDATION_MSG =
       "Address format should be [City name], [Street name  and number]";
 
   @NotBlank(message = "First name must not be blank, empty or null")
   @Size(min = 3, max = 64, message = "First name must size must be between 3 and 64")
-  @Pattern(regexp = "^(?!.*__)[A-Za-z0-9_]*$", message = NAME_PATTERN_VALIDATION_MSG)
+  @Pattern(regexp = NAME_REGEX, message = NAME_PATTERN_VALIDATION_MSG)
   private String firstName;
 
   @NotBlank(message = "Last name must not be blank, empty or null")
   @Size(min = 3, max = 64, message = "Last name size must be between 3 and 64")
-  @Pattern(regexp = "^(?!.*__)[A-Za-z0-9_]*$", message = NAME_PATTERN_VALIDATION_MSG)
+  @Pattern(regexp = NAME_REGEX, message = NAME_PATTERN_VALIDATION_MSG)
   private String lastName;
 
   @NotBlank(message = "Email must not be blank, empty or null")
@@ -47,10 +48,10 @@ public class UserRequestDto {
   @Pattern(regexp = "^[A-Za-z]+, [A-Za-z0-9\\s]+$", message = ADDRESS_PATTERN_VALIDATION_MSG)
   private String address;
 
-  @Pattern(regexp = PHONE_REGEXP, message = PHONE_PATTERN_VALIDATION_MSG)
+  @Pattern(regexp = PHONE_REGEX, message = PHONE_PATTERN_VALIDATION_MSG)
   private String phone;
 
-  @Pattern(regexp = PHONE_REGEXP, message = PHONE_PATTERN_VALIDATION_MSG)
+  @Pattern(regexp = PHONE_REGEX, message = PHONE_PATTERN_VALIDATION_MSG)
   private String phone2;
 
   @DateTimeFormat(pattern = "dd/MM/yyyy")
