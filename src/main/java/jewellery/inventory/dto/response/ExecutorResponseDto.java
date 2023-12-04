b4970c1349f7cafab0014acadba4f64d07874881
Alekseeybg <alekseybg@gmail.com>
2023-12-04 16:43:29 +0200

    [feature-42-user-details-extension] Add Lombok annotations to ExecutorResponseDto and JsonFormat annotation to SaleResponseDto.

--- src/main/java/jewellery/inventory/dto/response/ExecutorResponseDto.java
+++ src/main/java/jewellery/inventory/dto/response/ExecutorResponseDto.java
@@ -1,7 +1,15 @@
 package jewellery.inventory.dto.response;
 
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ExecutorResponseDto {
   private UUID id;
   private String firstName;
