eba20aa6dc3bab5d2520e97d7cfb0b276cb0b29c
ignatIgnatov <ignatov_rs@abv.bg>
2023-12-06 14:39:36 +0200

    add dealPrice constraints

--- src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
@@ -17,6 +17,9 @@ public class ResourceInUserRequestDto {
   private static final String QUANTITY_MIN_VALUE_MSG = "Quantity should not be less than 1.";
   private static final String QUANTITY_DECIMAL_PLACES_MSG =
       "Quantity should not have more than 2 decimal places.";
+  private static final String PRICE_MIN_VALUE_MSG = "Price should not be negative number";
+  private static final String PRICE_DECIMAL_PLACES_MSG =
+          "Price should not have more than 2 decimal places.";
 
   @NotNull private UUID userId;
   @NotNull private UUID resourceId;
@@ -25,5 +28,7 @@ public class ResourceInUserRequestDto {
   @Digits(integer = 10, fraction = 2, message = QUANTITY_DECIMAL_PLACES_MSG)
   private double quantity;
   @NotNull
+  @Min(value = 0, message = PRICE_MIN_VALUE_MSG)
+  @Digits(integer = 10, fraction = 2, message = PRICE_DECIMAL_PLACES_MSG)
   private double dealPrice;
 }
