7f30c4a1dda6808ab502f42e093cdb60d38bfc9a
berki0 <107307903+berki0@users.noreply.github.com>
2023-12-08 12:53:10 +0200

    [feature-47-database-backup] add DB backup script

---
+++ db_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DB_NAME=${JMS_DATABASE_NAME}
+DB_USER=${JMS_DATABASE_USER}
+DB_PASSWORD=${JMS_DATABASE_PASSWORD}
+
+case "$(uname -s)" in
+    Linux*|Darwin*)
+        BACKUP_DIR="/tmp/jms/db_backup"
+        ;;
+    MINGW*|CYGWIN*|MSYS*)
+        BACKUP_DIR="/c/tmp/jms/db_backup"
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
+esac
+
+mkdir -p "$BACKUP_DIR"
+
+DATE=$(date +"%Y%m%d_%H%M%S")
+FILE_NAME="db_backup_${DATE}.sql"
+
+export PGPASSWORD="$DB_PASSWORD"
+
+pg_dump -U "$DB_USER" -h localhost "$DB_NAME" > "$BACKUP_DIR/$FILE_NAME"
+echo "Database backup saved to $BACKUP_DIR/$FILE_NAME"
