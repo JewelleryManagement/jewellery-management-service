0f51460ea0cd1a8f143a75f603c7bcf260fd78b3
ignatIgnatov <ignatov_rs@abv.bg>
2023-12-19 10:46:08 +0200

    fix resource quantity decimal places

--- src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
@@ -13,10 +13,13 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class ResourceInUserRequestDto {
   private static final String QUANTITY_MIN_VALUE_MSG = "Quantity should not be less than 0.01.";
+  private static final String QUANTITY_DECIMAL_PLACES_MSG =
+          "Quantity should not have more than 2 decimal places.";
 
   @NotNull private UUID userId;
   @NotNull private UUID resourceId;
 
   @DecimalMin(value = "0.01", message = QUANTITY_MIN_VALUE_MSG)
+  @Digits(integer = 10, fraction = 2, message = QUANTITY_DECIMAL_PLACES_MSG)
   private double quantity;
 }
