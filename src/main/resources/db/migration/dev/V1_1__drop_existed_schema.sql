a6094397a8a32e1fbc87343a3afc3dc5bb0f224e
ignatIgnatov <ignatov_rs@abv.bg>
2023-12-20 11:16:59 +0200

    experimental commit -> drop schema before init and update system event type check

---
+++ src/main/resources/db/migration/dev/V1_1__drop_existed_schema.sql
@@ -0,0 +1,10 @@
+-- drop all existed tables before init
+DROP TABLE IF EXISTS system_event;
+DROP TABLE IF EXISTS product_author;
+DROP TABLE IF EXISTS resource_in_product;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS sale;
+DROP TABLE IF EXISTS resource_in_user;
+DROP TABLE IF EXISTS resource;
+DROP TABLE IF EXISTS users;
