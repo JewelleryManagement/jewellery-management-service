f0ea379e55762c641b9c355fe1a820e3f6148487
ignatIgnatov <ignatov_rs@abv.bg>
2023-12-21 14:32:11 +0200

    add final String MINIMAL_RESOURCE_QUANTITY in ResourceInUserRequestDto

--- src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 public class ResourceInUserRequestDto {
+  private static final String MINIMAL_RESOURCE_QUANTITY = "0.01";
   private static final String QUANTITY_MIN_VALUE_MSG = "Quantity should not be less than 0.01.";
   private static final String QUANTITY_DECIMAL_PLACES_MSG =
           "Quantity should not have more than 2 decimal places.";
@@ -19,7 +20,7 @@ public class ResourceInUserRequestDto {
   @NotNull private UUID userId;
   @NotNull private UUID resourceId;
 
-  @DecimalMin(value = "0.01", message = QUANTITY_MIN_VALUE_MSG)
+  @DecimalMin(value = MINIMAL_RESOURCE_QUANTITY, message = QUANTITY_MIN_VALUE_MSG)
   @Digits(integer = 10, fraction = 2, message = QUANTITY_DECIMAL_PLACES_MSG)
   private double quantity;
 }
