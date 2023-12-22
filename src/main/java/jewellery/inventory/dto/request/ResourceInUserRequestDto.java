5c7a32048c57c9670ad697486212807e5316a78e
ignatIgnatov <ignatov_rs@abv.bg>
2023-12-22 14:48:16 +0200

    change @DecimalMin with @Positive in ResourceInUserRequestDto and ResourceQuantityRequestDto, remove migration for system_event_type_check

--- src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/ResourceInUserRequestDto.java
@@ -12,15 +12,10 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 public class ResourceInUserRequestDto {
-  private static final String MINIMAL_RESOURCE_QUANTITY = "0.01";
-  private static final String QUANTITY_MIN_VALUE_MSG = "Quantity should not be less than 0.01.";
-  private static final String QUANTITY_DECIMAL_PLACES_MSG =
-          "Quantity should not have more than 2 decimal places.";
 
   @NotNull private UUID userId;
   @NotNull private UUID resourceId;
 
-  @DecimalMin(value = MINIMAL_RESOURCE_QUANTITY, message = QUANTITY_MIN_VALUE_MSG)
-  @Digits(integer = 10, fraction = 2, message = QUANTITY_DECIMAL_PLACES_MSG)
+  @Positive
   private double quantity;
 }
--- src/main/java/jewellery/inventory/dto/request/resource/ResourceQuantityRequestDto.java
+++ src/main/java/jewellery/inventory/dto/request/resource/ResourceQuantityRequestDto.java
@@ -1,7 +1,6 @@
 package jewellery.inventory.dto.request.resource;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Positive;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +11,7 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class ResourceQuantityRequestDto {
 
-    private static final String MINIMAL_RESOURCE_QUANTITY = "0.01";
-    private static final String RESOURCE_QUANTITY_MESSAGE = "Resource quantity should not be less than 0.01";
-    private static final String QUANTITY_DECIMAL_PLACES_MSG =
-            "Resource quantity should not have more than 2 decimal places.";
-
     private UUID id;
-    @DecimalMin(value = MINIMAL_RESOURCE_QUANTITY, message = RESOURCE_QUANTITY_MESSAGE)
-    @Digits(integer = 10, fraction = 2, message = QUANTITY_DECIMAL_PLACES_MSG)
+    @Positive
     private double quantity;
 }
