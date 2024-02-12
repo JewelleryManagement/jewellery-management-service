0fefa6bcc07398d85184a062e5360c0f8d7c80dc
berki0 <107307903+berki0@users.noreply.github.com>
2024-02-12 20:54:32 +0200

    [Feature-58-products-calculated-price] migrations - db/migration/dev

--- src/main/resources/db/migration/prod/V1_3__create_product_price_discount_Table.sql
+++ src/main/resources/db/migration/prod/V1_3__create_product_price_discount_Table.sql
@@ -1,9 +1,8 @@
 ALTER TABLE public.product
-ADD COLUMN additional_price DECIMAL(10, 2);
-
-ALTER TABLE public.product
-DROP COLUMN sale_price CASCADE,
-DROP COLUMN discount CASCADE;
+ADD COLUMN additional_price DECIMAL(10, 2),
+DROP COLUMN IF EXISTS part_of_sale_id CASCADE,
+DROP COLUMN IF EXISTS sale_price CASCADE,
+DROP COLUMN IF EXISTS discount CASCADE;
 
 CREATE TABLE public.product_price_discount (
     id UUID PRIMARY KEY,
@@ -14,3 +13,6 @@ CREATE TABLE public.product_price_discount (
     FOREIGN KEY (product_id) REFERENCES product(id),
     FOREIGN KEY (sale_id) REFERENCES sale(id)
 );
+ALTER TABLE public.product
+ADD COLUMN IF NOT EXISTS part_of_sale_id UUID,
+ADD CONSTRAINT fk_product_price_discount_id FOREIGN KEY (part_of_sale_id) REFERENCES public.product_price_discount(id);
