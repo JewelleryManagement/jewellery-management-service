554c2cb6ba8270c6db96cae74490a80d6c640426
ignatIgnatov <ignatov_rs@abv.bg>
2024-02-16 15:02:21 +0200

    refactoring

--- src/main/resources/db/migration/dev/V1_1__drop_existed_schema.sql
+++ src/main/resources/db/migration/dev/V1_1__drop_existed_schema.sql
@@ -2,10 +2,8 @@
 DROP TABLE IF EXISTS system_event;
 DROP TABLE IF EXISTS product_author;
 DROP TABLE IF EXISTS resource_in_product;
-DROP TABLE IF EXISTS product_price_discount;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS purchased_resources;
 DROP TABLE IF EXISTS sale;
 DROP TABLE IF EXISTS resource_in_user;
 DROP TABLE IF EXISTS resource;
