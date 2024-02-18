e75b5a5bf2f12fc352e94f9c45cc9471e474ae25
berki0 <107307903+berki0@users.noreply.github.com>
2024-02-18 02:55:47 +0200

    [Feature-65-expose-organization-create/read-endpoint] - add tests

---
+++ src/test/java/jewellery/inventory/integration/OrganizationCrudIntegrationTest.java
@@ -0,0 +1,68 @@
+package jewellery.inventory.integration;
+
+import static jewellery.inventory.helper.OrganizationTestHelper.getTestOrganization;
+import static jewellery.inventory.helper.OrganizationTestHelper.getTestOrganizationRequest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import jewellery.inventory.dto.request.OrganizationRequestDto;
+import jewellery.inventory.dto.response.OrganizationResponseDto;
+import jewellery.inventory.dto.response.ProductResponseDto;
+import jewellery.inventory.model.Organization;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+class OrganizationCrudIntegrationTest extends AuthenticatedIntegrationTestBase {
+  private String getBaseOrganizationsUrl() {
+    return "/organizations";
+  }
+  private String getOrganizationByIdUrl(UUID id) {
+    return "/products/" + id;
+  }
+
+  private Organization organization;
+  private OrganizationRequestDto organizationRequestDto;
+
+  @BeforeEach
+  void setUp() {
+    organization = getTestOrganization();
+    organizationRequestDto = getTestOrganizationRequest();
+  }
+
+    @Test
+    void getOrganizationsSuccessfully() {
+      ResponseEntity<List<ProductResponseDto>> response =
+              this.testRestTemplate.exchange(
+                      getBaseOrganizationsUrl(), HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+
+      assertEquals(response.getStatusCode(),HttpStatusCode.valueOf(200) );
+      assertNotNull(response.getBody());
+  }
+
+  @Test
+  void getOrganizationByIdNotFound() {
+    ResponseEntity<OrganizationResponseDto> response =
+        this.testRestTemplate.getForEntity(
+                getOrganizationByIdUrl(Objects.requireNonNull(organization).getId()),
+            OrganizationResponseDto.class);
+
+    assertEquals(response.getStatusCode(),HttpStatusCode.valueOf(404));
+}
+
+  @Nullable
+  private OrganizationResponseDto createOrganizationsWithRequest(OrganizationRequestDto dto) {
+    ResponseEntity<OrganizationResponseDto> response =
+        this.testRestTemplate.postForEntity(
+            getBaseOrganizationsUrl(), dto, OrganizationResponseDto.class);
+
+    return response.getBody();
+  }
+}
