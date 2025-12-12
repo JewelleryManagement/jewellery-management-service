de6eadbf1ba8b8ea4008f6fb95d2907e7ae23a90
Lubomir <lubomir.karnatov@gmail.com>
2025-12-12 14:40:58 +0200

    fix: add update_resources_and_insert_allowed_values migration to common

--- src/main/resources/db/migration/dev/V1_11_1__update_resource_field_values.sql
+++ src/main/resources/db/migration/dev/V1_11_1__update_resource_field_values.sql
@@ -1,7 +1,3 @@
-UPDATE allowed_value
-SET sku = 'test'
-WHERE sku IS NULL;
-
 UPDATE public.resource SET sku = 'P.C.0.00' WHERE id = '6ea11215-db8f-4b5a-a5dd-1fa748059655';
 UPDATE public.resource SET sku = 'M.C.0' WHERE id = '9f76ddce-b01d-451b-add2-4218fd87d358';
 UPDATE public.resource SET sku = 'P.0' WHERE id = '4d3b076c-b8da-48ab-bfd8-fb50a470e922';
@@ -13,15 +9,7 @@ SET dtype = 'Diamond',
     clazz = 'Diamond'
 WHERE dtype = 'PreciousStone' OR clazz = 'PreciousStone';
 
-UPDATE public.allowed_value
-SET resource_clazz = 'Diamond'
-WHERE resource_clazz = 'PreciousStone';
-
 UPDATE public.resource
 SET dtype = 'DiamondMelee',
     clazz = 'DiamondMelee'
 WHERE dtype = 'SemiPreciousStone' OR clazz = 'SemiPreciousStone';
-
-UPDATE public.allowed_value
-SET resource_clazz = 'DiamondMelee'
-WHERE resource_clazz = 'SemiPreciousStone';
